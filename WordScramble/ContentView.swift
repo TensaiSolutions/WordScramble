a58d9254b2103b488dbf0b0e066ac01497ec8286
Tensai Solutions <sidellp@gmail.com>
2023-10-16 23:32:34 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// WordScramble
+//
+// Created by Tensai Solutions on 10/16/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,18 @@
+//
+// WordScrambleApp.swift
+// WordScramble
+//
+// Created by Tensai Solutions on 10/16/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
